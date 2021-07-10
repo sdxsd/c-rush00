b04fa7f9523506ba87ca5b24aea82abe36837730
Mart Broerse <mbroerse@f1r5s20.codam.nl>
2021-07-10 19:36:19 +0200

    found out a way to omit a function by using pointers

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -1,24 +1,13 @@
 #include <unistd.h>
 void	ft_putchar(char c);
-void	charmap(int x);
-
-void	charmap(int x)
-{
-	char	square_chars[5];
-
-	square_chars[0] = 'A';
-	square_chars[1] = 'B';
-	square_chars[2] = 'C';
-	square_chars[3] = ' ';
-	square_chars[4] = '\n';
-	ft_putchar(square_chars[x]);
-}
 
 void	rush(int x, int y)
 {
+	char	*square_chars;
 	int		i_x;
 	int		i_y;
 
+	square_chars = "ABC \n";
 	i_x = 0;
 	i_y = 0;
 	while (i_y < y)
@@ -26,17 +15,17 @@ void	rush(int x, int y)
 		while (i_x < x)
 		{
 			if ((i_y == 0 || i_y == y - 1) != (i_x == 0 || i_x == x - 1))
-				charmap(1);
+				ft_putchar(square_chars[1]);
 			else if (i_x == 0)
-				charmap(0);
+				ft_putchar(square_chars[0]);
 			else if (i_x == x - 1)
-				charmap(2);
+				ft_putchar(square_chars[2]);
 			else
-				charmap(3);
+				ft_putchar(square_chars[3]);
 			i_x++;
 		}
 		i_x = 0;
 		i_y++;
-		charmap(4);
+		ft_putchar(square_chars[4]);
 	}
 }
