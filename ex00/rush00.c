663c8cf37bfc72d69dafc417054506f3a4ea2a5d
Mart Broerse <mbroerse@f1r5s20.codam.nl>
2021-07-10 16:15:21 +0200

    first working version, too many lines, logic could be neater

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -1,30 +1,47 @@
 #include <unistd.h>
-
+#include <stdlib.h>
 void	ft_putchar(char c);
-
-// Example square formatting. 
-//01110
-//2   2
-//2   2
-//01110
 
 void	rush(int x, int y)
 {
 	int		i_x;
 	int		i_y;
-	char	square_chars[];
+	char	square_chars[5];
 
-	square_chars = {'o', '-', '|'};
+	square_chars[0] = 'A';
+	square_chars[1] = 'B';
+	square_chars[2] = 'C';
+	square_chars[3] = ' ';
+	square_chars[4] = '\n';
 	i_x = 0;
 	i_y = 0;
-	while (i_x <= i_y)
+	while (i_y < y)
 	{
-		while (i_x <= i_y)
+		while (i_x < x)
 		{
-			ft_putchar(&corner);
+			if (i_y == 0 || i_y == y - 1)
+			{
+				if (i_x == 0)
+				{
+					ft_putchar(square_chars[0]);
+				}
+				else if (i_x == x - 1)
+				{
+					ft_putchar(square_chars[2]);
+				}
+				else
+					ft_putchar(square_chars[1]);
+			}
+			else if (i_x == 0 || i_x == x - 1)
+			{
+				ft_putchar(square_chars[1]);
+			}
+			else
+				ft_putchar(square_chars[3]);
 			i_x++;
 		}
-		i_y = 0;
+		i_x = 0;
 		i_y++;
+		ft_putchar(square_chars[4]);
 	}
 }
